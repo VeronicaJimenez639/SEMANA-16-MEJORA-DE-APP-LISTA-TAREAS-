b074b4921a14aca31a4ad64e7999e5001b4466ec
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-04-02 19:52:52 -0500

    Se probó el correcto  funcionamiento del programa

--- lista_tareas_app/main.py
+++ lista_tareas_app/main.py
@@ -6,4 +6,4 @@ from ui.app_tkinter import AppTkinter
 if __name__ == "__main__":
     ventana_principal = tk.Tk()
     aplicacion = AppTkinter(ventana_principal)
-    ventana_principal.mainloop()
+    ventana_principal.mainloop() 
